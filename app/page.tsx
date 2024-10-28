04782935cd0161e4ec8661c08cf9c0ae6f8eb0e5
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-10-28 10:26:38 +0700

    Add multiple charts

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import { SyntheticChart } from "./_components/synthetic-chart";
 
 export default function Home() {
   return (
-    <div>
-      <SyntheticChart />
+    <div className="p-4">
+      <div className="grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
+        <SyntheticChart />
+        <SyntheticChart />
+        <SyntheticChart />
+      </div>
     </div>
   );
 }
